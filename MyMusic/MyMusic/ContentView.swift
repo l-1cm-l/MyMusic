7b6100057adb3420a6ad55594f88856e0b4a9925
l-1cm-l <l-----1.5cm----l@i.softbank.jp>
2021-03-20 09:28:46 +0900

    [practice]

--- MyMusic/MyMusic/ContentView.swift
+++ MyMusic/MyMusic/ContentView.swift
@@ -14,9 +14,23 @@ struct ContentView: View {
                 .resizable()
                 .edgesIgnoringSafeArea(.all)
                 .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+        }//ZStackここまで
+            HStack{
+                Button(action:{
+                    //ボタンをタップした時のアクション
+                }){
+                    Image("cymbal")
+                        .renderingMode(.original)
+                }
+                Button(action:{
+                    //ボタンをタップした時のアクション
+                }){
+                    Image("guitar")
+                        .renderingMode(.original)
+                }
+                }
+            }
         }
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
